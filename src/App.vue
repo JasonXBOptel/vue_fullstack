bfd47c0774e15e7814574c277d2198ff832dc177
JasonXB <jasonxportfolio@gmail.com>
2023-01-23 20:21:32 -0500

    Array Change Mutation Testing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script setup>
-// import { ref } from "vue";
-const arr = [
+import { ref } from "vue";
+const efRef = ref([
   { name: "Jason", age: 26 },
   { name: "Kelsey", age: 30 },
   { name: "Roland", age: 32 },
-];
+]);
+
+const extendStateArray = function () {
+  efRef.value.push({ name: "Shiho", age: 24 });
+};
 </script>
 <template>
   <div>
+    <button @click="() => efRef.push({ name: 'Shiho', age: 24 })">
+      Extend array- use ref in template block
+    </button>
+    <button @click="extendStateArray">
+      Extend array- use ref in script block
+    </button>
     <ul>
-      <li v-for="({ name, age }, index) in arr" :key="index">
+      <li v-for="({ name, age }, index) in efRef" :key="index">
         <h4>Name: {{ name }}</h4>
         <h6>Age: {{ age }}</h6>
       </li>
