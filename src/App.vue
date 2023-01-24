b7cb7d6321f23f45b5b5844c910e358f5601de60
JasonXB <jasonxportfolio@gmail.com>
2023-01-24 10:33:42 -0500

    Method Output JSX

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,25 @@
 <script setup>
-import { ref, computed } from "vue";
-// Declare stateful array
-const cart = ref([
-  {
-    item: "Wire tray",
-    price: 26,
-  },
-  {
-    item: "USB-C adaptor",
-    price: 22,
-  },
-  {
-    item: "Extension Cable",
-    price: 14,
-  },
-]);
-// Create computed value that auto-updates when cart stateful value changes
-const cartTotal = computed(() => {
-  return cart.value.reduce((acc, curr) => acc + curr.price, 0);
-});
-// Calc a value normally (no auto-updates)
-const staticCartTotal = cart.value.reduce((acc, curr) => acc + curr.price, 0);
-// Function that adds entries to the stateful array
-const addToCart = function () {
-  cart.value.push({ item: "Eraser", price: 2 });
-  console.log(cart.value);
+// import { ref, computed } from "vue";
+// import { v4 as uuidv4 } from "uuid";
+
+const obj = {
+  name: "Spinel",
+  color: "Pink",
+  weapon: "Scythe",
+  sanity: 0,
+};
+console.log(crypto.randomUUID());
+const complexLogic = function (value, property) {
+  console.log([value, property]);
+  return value;
 };
 </script>
 <template>
   <div>
     <ul>
-      <li v-for="(entry, i) of cart" :key="i">
-        <h4>item: {{ entry.item }}</h4>
-        <h6>price: {{ entry.price }}</h6>
+      <li v-for="(value, property) of obj" :key="property">
+        {{ complexLogic(value, property) }}
       </li>
-      <h2>TOTAL: {{ cartTotal }}</h2>
-      <button @click="addToCart">Add to cart</button>
     </ul>
   </div>
 </template>
