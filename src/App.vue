a9d4f9015b90fc15270ec7d3513e61785baad39b
JasonXB <jasonxportfolio@gmail.com>
2023-01-25 23:17:28 -0500

    Call API on startup- async await

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
 import { ref, watch, onMounted } from "vue";
 import axios from "axios";
 // Declare stateful value, which gets changed by pressing a button
-const pageNum = ref(1);
+// const pageNum = ref(1);
 const results = ref({});
 
 onMounted(() => {
-  axios
-    .get(`https://jsonplaceholder.typicode.com/posts/${pageNum.value}`)
-    .then((resp) => resp.data)
-    .then((parsedData) => {
-      results.value = parsedData;
-    })
-    .catch((error) => {
+  const callFN = async function () {
+    try {
+      const resp = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/1"
+      );
+      const parsed = resp.data;
+      results.value = parsed;
+    } catch (error) {
       console.error(error);
-    });
+    }
+  };
+  callFN();
 });
 </script>
 
@@ -23,7 +26,7 @@ onMounted(() => {
     <section>
       <!-- <button @click="pageNum++">Increment pageNum</button> -->
       <h1>API CALL RESULTS BELOW</h1>
-      <h3>State Val: {{ pageNum }}</h3>
+      <!-- <h3>State Val: {{ pageNum }}</h3> -->
       <p>{{ results }}</p>
     </section>
   </div>
