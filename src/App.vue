07a417162d038fe0e020446fef53d5df3955a496
JasonXB <jasonxportfolio@gmail.com>
2023-01-25 23:33:34 -0500

    watch test with api call

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch } from "vue";
 import axios from "axios";
 // Declare stateful value, which gets changed by pressing a button
 const pageNum = ref(1);
 const results = ref({});
 
+const fetchThenUpdate = async function (num) {
+  try {
+    const resp = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${num}`
+    );
+    results.value = resp.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Fetch API data on Startup then render the results
 onMounted(() => {
-  const callFN = async function () {
-    try {
-      const resp = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts/1"
-      );
-      results.value = resp.data;
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  callFN();
+  fetchThenUpdate(pageNum.value);
+});
+// Run callback that fetches data whenever pageNum reactive value changes
+watch(pageNum, async function (newVal, oldVal) {
+  console.log([oldVal, "->", newVal]);
+  try {
+    await fetchThenUpdate(pageNum.value);
+  } catch (error) {
+    console.error(error);
+  }
 });
 </script>
 
