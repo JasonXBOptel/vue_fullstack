123c49b0023b158b93e5564e110839f0355cadba
JasonXB <jasonxportfolio@gmail.com>
2023-01-26 13:56:43 -0500

    v-bind test 1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,21 @@
 <script setup>
-import { ref, onMounted, watch, watchEffect } from "vue";
-import axios from "axios";
-// Declare stateful value, which gets changed by pressing a button
-const pageNum = ref(1);
-const results = ref({});
+import { ref } from "vue";
+const goBoolean = ref(true);
 
-const fetchThenUpdate = async function (num) {
-  try {
-    const resp = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts/${num}`
-    );
-    results.value = resp.data;
-  } catch (error) {
-    console.error(error);
-  }
+const decideColor = () => {
+  if (goBoolean.value) return "green";
+  else return "red";
 };
 
-// Callback runs on startup by default
-// Run callback that fetches data whenever pageNum reactive value changes
-watchEffect(async function () {
-  try {
-    await fetchThenUpdate(pageNum.value); // involves results reactive variable
-  } catch (error) {
-    console.error(error);
-  }
-});
+console.log(goBoolean);
 </script>
 
 <template>
-  <div id="app">
-    <section>
-      <button @click="pageNum++">Increment pageNum</button>
-      <h3>State Val: {{ pageNum }}</h3>
-      <h3>API CALL RESULTS BELOW</h3>
-      <p>{{ results }}</p>
-    </section>
-  </div>
+  <h1>hello</h1>
 </template>
 
 <style scoped>
-button {
-  font-size: 1.5rem;
-  margin: 1rem;
-  margin-left: 0;
+h1 {
+  color: v-bind(decideColor());
 }
 </style>
