a5b4cb1f3dacb9daab82d343bd6ca926806cd439
JasonXB <jasonxportfolio@gmail.com>
2023-01-28 15:49:16 -0500

    v-model with radio buttons

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <script setup>
 import { ref } from "vue";
-// Create a state variable to reference when using v-model
-const message = ref("");
+// Default value: A
+const picked = ref("A"); // ref value will be set to value attribute
 </script>
 
 <template>
+  <div>Picked: {{ picked }}</div>
+
   <section>
-    <p>message (reactive variable): {{ message }}</p>
-    <input v-model="message" placeholder="edit me" />
+    <input type="radio" value="A" v-model="picked" />
+    <label for="one">Option A</label>
+  </section>
+
+  <section>
+    <input type="radio" value="B" v-model="picked" />
+    <label for="two">Option B</label>
+  </section>
+
+  <section>
+    <input type="radio" value="C" v-model="picked" />
+    <label for="two">Option C</label>
   </section>
 </template>
-<style>
+
+<style scoped>
 section {
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column; */
+}
+section input,
+p {
+  margin-left: 2rem;
 }
 </style>
