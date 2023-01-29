b2011fe4d165ff419cdd691794b5e48f86a41406
JasonXB <jasonxportfolio@gmail.com>
2023-01-29 15:26:27 -0500

    Props showcase

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
+<!-- eslint-disable no-unused-vars -->
 <script setup>
-import { ref } from "vue";
-// Create a state variable to reference when using v-model
-const typedInput = ref(""); // default value is an empty string
+// import { ref } from "vue";
+import CustomComp from "./split/CustomComp.vue";
+const obj = {
+  name: "Munroop",
+  main: "Fox",
+};
+const list = [
+  { name: "Jason", age: 26 },
+  { name: "Kelsey", age: 30 },
+  { name: "Roland", age: 32 },
+];
 </script>
-
-<template src="./split/template.html"></template>
+<!-- Import the split HTML and CSS files -->
+<template>
+  <CustomComp stringVal="Filler text" numVal="43" :obj="obj" :list="list" />
+</template>
 <style src="./split/style.css"></style>
