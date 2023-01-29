37c4146bcfda69fbac00abe3126465074bd4e0b1
JasonXB <jasonxportfolio@gmail.com>
2023-01-29 16:39:07 -0500

    Hardcoded non string props

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,16 @@
 import CustomComp from "./split/CustomComp.vue";
 </script>
 <!-- v-bind hardcoded non-string props -->
+<!-- If you don't specify a prop value, it get auto-set to true -->
 <template>
-  <CustomComp trueVal :falseVal="false" :numVal="43" />
+  <CustomComp
+    trueVal
+    :falseVal="false"
+    :numVal="43"
+    :specificNumVal="0"
+    :nullishVal1="null"
+    :nullishVal2="undefined"
+    :dateVal="new Date()"
+  />
 </template>
 <style src="./split/style.css"></style>
