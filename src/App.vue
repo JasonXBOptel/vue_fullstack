e9b83e58a84ceb422ec57dda73d57ed109adc4b7
JasonXB <jasonxportfolio@gmail.com>
2023-01-28 19:38:33 -0500

    v-number modifier showcase

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,19 @@
 <script setup>
 import { ref } from "vue";
-// Default value: A
-const picked = ref("A"); // ref value will be set to value attribute
+// Create a state variable to reference when using v-model
+const typedInput = ref(""); // default value is an empty string
 </script>
 
 <template>
-  <div>Picked: {{ picked }}</div>
-
   <section>
-    <input type="radio" value="A" v-model="picked" />
-    <label for="one">Option A</label>
-  </section>
-
-  <section>
-    <input type="radio" value="B" v-model="picked" />
-    <label for="two">Option B</label>
-  </section>
-
-  <section>
-    <input type="radio" value="C" v-model="picked" />
-    <label for="two">Option C</label>
+    <p>Reactive val: {{ typeof typedInput }}</p>
+    <input v-model.number="typedInput" placeholder="edit me" />
   </section>
 </template>
 
 <style scoped>
 section {
   display: flex;
-  /* flex-direction: column; */
-}
-section input,
-p {
-  margin-left: 2rem;
+  flex-direction: column;
 }
 </style>
