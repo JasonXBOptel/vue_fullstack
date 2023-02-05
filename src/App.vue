15cce1d62900a16da20c1b836f912185a35d3905
Jason Bustamante <jason.bustamante@optelgroup.com>
2023-02-04 20:06:35 -0500

    State arrays

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,43 @@
 <template lang="">
   <!-- Can access the state value directly in template block -->
-  <h2>Jason Stats: {{ grades }}</h2>
-  <button @click="jasonPowerup">Add entry to end</button>
-  <button @click="jasonPowerup">Remove entry from start</button>
-  <button @click="jasonPowerup">Remove second half</button>
+  <section>
+    <h2>Jason Stats: {{ grades }}</h2>
+    <button @click="addToEnd(50)">Add entry to end</button>
+    <button @click="removeFromStart">Remove entry from start</button>
+    <button @click="halve">Remove second half</button>
+    <button @click="calcAverage">Log average</button>
+  </section>
 </template>
 <script>
-let regularVar = 9;
+// let regularVar = 9;
 export default {
-  methods: {
-    addToEnd() {},
-    removeFromStart() {},
-    halve() {},
-    calcAverage() {},
-  },
   data() {
     return {
       grades: [55, 70, 90, 60, 40, 62, 65],
     };
   },
+  methods: {
+    addToEnd(i) {
+      this.grades.push(i);
+    },
+    removeFromStart() {
+      this.grades.shift();
+    },
+    halve() {
+      const halfwayPt = Math.floor(this.grades.length / 2);
+      this.grades.splice(halfwayPt);
+    },
+    calcAverage() {
+      let arr = this.grades;
+      let sum = arr.reduce((acc, currVal) => acc + currVal, 0);
+      console.log(sum / this.grades.length);
+    },
+  },
 };
 </script>
-<style lang=""></style>
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+}
+</style>
