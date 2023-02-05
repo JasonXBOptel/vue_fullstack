85cd0db61e98fc0191b06a5c1d2fce560b0ed05d
Jason Bustamante <jason.bustamante@optelgroup.com>
2023-02-04 23:50:57 -0500

    Watchers test

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,34 @@
-<template lang="">
-  <!-- Can access the state value directly in template block -->
-  <section>
-    <h2>Jason Stats: {{ grades }}</h2>
-    <button @click="addToEnd(50)">Add entry to end</button>
-    <button @click="removeFromStart">Remove entry from start</button>
-    <button @click="halve">Remove second half</button>
-    <button @click="calcAverage">Log average</button>
-  </section>
-</template>
 <script>
-// let regularVar = 9;
+import axios from "axios";
 export default {
   data() {
-    return {
-      grades: [55, 70, 90, 60, 40, 62, 65],
-    };
+    return { page: 1, results: [] };
   },
-  methods: {
-    addToEnd(i) {
-      this.grades.push(i);
-    },
-    removeFromStart() {
-      this.grades.shift();
-    },
-    halve() {
-      const halfwayPt = Math.floor(this.grades.length / 2);
-      this.grades.splice(halfwayPt);
-    },
-    calcAverage() {
-      let arr = this.grades;
-      let sum = arr.reduce((acc, currVal) => acc + currVal, 0);
-      console.log(sum / this.grades.length);
+  method: {},
+  watch: {
+    page: {
+      // run watcher on startup (like useEffect with empty dep list)
+      immediate: true,
+      // When page state value changes, fetch new data from the API
+      handler() {
+        axios
+          .get("https://jsonplaceholder.typicode.com/posts/" + this.page)
+          .then((resp) => (this.results = resp.data))
+          .catch((err) => console.error(err));
+      },
     },
   },
 };
 </script>
+
+<template>
+  <div id="app">
+    <p>{{ results }}</p>
+    <h3>Page Number: {{ page }}</h3>
+    <button @click="page++">Page forward</button>
+    <button @click="page--">Page back</button>
+  </div>
+</template>
 <style scoped>
 section {
   display: flex;
