ce14c68e53f70a94c91dc39f24d802dc1cea4320
Jason Bustamante <jason.bustamante@optelgroup.com>
2023-02-06 01:05:13 -0500

    created Options API Lifecycle

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,25 @@
-<script setup>
-import { ref, watch, onMounted } from "vue";
-import axios from "axios";
-// Declare stateful value, which gets changed by pressing a button
-// const pageNum = ref(1);
-const results = ref({});
-
-onMounted(() => {
-  axios
-    .get(`https://jsonplaceholder.typicode.com/posts/1`)
-    .then((resp) => resp.data)
-    .then((parsedData) => (results.value = parsedData))
-    .catch((error) => {
-      console.error(error);
-    });
-});
-</script>
-
-<template>
-  <div id="app">
-    <section>
-      <!-- <button @click="pageNum++">Increment pageNum</button> -->
-      <h1>API CALL RESULTS BELOW</h1>
-      <!-- <h3>State Val: {{ pageNum }}</h3> -->
-      <p>{{ results }}</p>
-    </section>
-  </div>
+<template lang="">
+  <div>{{ results }}</div>
 </template>
-
-<style scoped>
-button {
-  font-size: 1.5rem;
-  margin: 1rem;
-  margin-left: 0;
-}
-</style>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      results: [],
+    };
+  },
+  created() {
+    // pretend this is a backend endpoint (process would be the same)
+    let endpoint = "https://jsonplaceholder.typicode.com/posts/1";
+    axios
+      .get(endpoint)
+      .then((resp) => resp.data)
+      .then((parsedData) => (this.results = parsedData))
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
+</script>
+<style lang=""></style>
