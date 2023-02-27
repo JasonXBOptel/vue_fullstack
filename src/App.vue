b64d25f59eed48411f00c545f6f3a2cf7e97d141
Jason Bustamante <jason.bustamante@optelgroup.com>
2023-02-27 01:07:16 -0500

    Multiple Scoped Name Slots in a List

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <script>
 import CustomComp from "./split/CustomComp.vue";
+
 export default {
   components: { CustomComp },
   data() {
-    return { var1: "Stateful message" };
+    return { first: "uno", second: "dos", third: "tres" };
   },
 };
 </script>
 
 <template>
-  <CustomComp v-slot="slotProps">
-    <h1>{{ slotProps.title }}</h1>
-    <p>{{ slotProps.text }}</p>
+  <CustomComp api-url="url" v-bind:per-page="10">
+    <template v-slot:listObj="{ body, username, likes }">
+      <div>
+        <p>{{ body }}</p>
+        <p class="meta">by {{ username }}</p>
+        <p>Number of likes: {{ likes }}</p>
+      </div>
+    </template>
   </CustomComp>
 </template>
+
+<!-- <style scoped>
+.meta {
+  font-size: 0.8em;
+  color: #42b883;
+}
+</style> -->
--- src/split/CustomComp.vue
+++ src/split/CustomComp.vue
@@ -1,14 +1,34 @@
 <script>
 export default {
+  props: ["api-url", "per-page"],
   data() {
-    return { titleState: "Stateful title" };
+    return {
+      items: [
+        { body: "Scoped Slots Guide", username: "Evan You", likes: 20 },
+        { body: "Vue Tutorial", username: "Natalia Tepluhina", likes: 10 },
+      ],
+    };
   },
 };
 </script>
+
 <template>
-  <slot
-    v-bind:title="titleState"
-    v-bind:text="'Insert random hardcoded paragraph here'"
-  >
-  </slot>
+  <ul>
+    <li v-for="(listObj, index) in items" v-bind:key="index">
+      <slot name="listObj" v-bind="listObj" />
+    </li>
+  </ul>
 </template>
+
+<!-- <style scoped>
+ul {
+  list-style-type: none;
+  padding: 5px;
+  background: linear-gradient(315deg, #42d392 25%, #647eff);
+}
+li {
+  padding: 5px 20px;
+  margin: 10px;
+  background: #fff;
+}
+</style> -->
