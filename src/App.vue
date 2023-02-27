06e2f761e8db2ccab3b23fdaecc79cd425ebde99
Jason Bustamante <jason.bustamante@optelgroup.com>
2023-02-27 01:21:32 -0500

    Multiple Scoped Name Slots one at a time

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,21 @@ import CustomComp from "./split/CustomComp.vue";
 
 export default {
   components: { CustomComp },
-  data() {
-    return { first: "uno", second: "dos", third: "tres" };
-  },
 };
 </script>
 
 <template>
-  <CustomComp api-url="url" v-bind:per-page="10">
-    <template v-slot:listObj="{ body, username, likes }">
-      <div>
-        <p>{{ body }}</p>
-        <p class="meta">by {{ username }}</p>
-        <p>Number of likes: {{ likes }}</p>
-      </div>
+  <CustomComp>
+    <template #header="headerProps">
+      {{ headerProps }}
+    </template>
+
+    <template #default="defaultProps">
+      {{ defaultProps }}
+    </template>
+
+    <template #footer="footerProps">
+      {{ footerProps }}
     </template>
   </CustomComp>
 </template>
-
-<!-- <style scoped>
-.meta {
-  font-size: 0.8em;
-  color: #42b883;
-}
-</style> -->
--- src/split/CustomComp.vue
+++ src/split/CustomComp.vue
@@ -2,33 +2,18 @@
 export default {
   props: ["api-url", "per-page"],
   data() {
-    return {
-      items: [
-        { body: "Scoped Slots Guide", username: "Evan You", likes: 20 },
-        { body: "Vue Tutorial", username: "Natalia Tepluhina", likes: 10 },
-      ],
-    };
+    return { first: "uno", second: "dos", third: "tres" };
   },
 };
 </script>
 
 <template>
-  <ul>
-    <li v-for="(listObj, index) in items" v-bind:key="index">
-      <slot name="listObj" v-bind="listObj" />
-    </li>
-  </ul>
+  <h4>Behold the props object for each of our named slots</h4>
+  <slot name="header" :message="first">Fallback text</slot>
+  <br />
+  <slot name="default" :message="second">Fallback text</slot>
+  <br />
+  <slot name="footer" :message="third">Fallback text</slot>
+  <br />
+  <slot name="footer" message="fourth hardcoded">Fallback text</slot>
 </template>
-
-<!-- <style scoped>
-ul {
-  list-style-type: none;
-  padding: 5px;
-  background: linear-gradient(315deg, #42d392 25%, #647eff);
-}
-li {
-  padding: 5px 20px;
-  margin: 10px;
-  background: #fff;
-}
-</style> -->
